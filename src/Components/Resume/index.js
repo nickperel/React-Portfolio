d48f40d6a83fad2bf09dc3bfacaf13b57cd05ed0
Kakashi <nickperel@live.com>
2022-03-19 01:20:47 -0400

    added to the Proficiencies list, updated resume link, & styled the Resume page

--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -2,24 +2,34 @@ import React from 'react';
 
 function Resume() {
     return (
-        <div className="container">
-            <a href='https://drive.google.com/file/d/1MJlzD45B7mdkQdg9GQGO9PLmKxm5Pudt/view?usp=sharing'>Link to Resume Here</a>
-            <h1 className='d-flex justify-content-center'>Proficiencies</h1>
+        <div className="container resume-container">
+            <button className="btn btn-info mt-5" href='https://drive.google.com/file/d/1Kax2iFRZAfOd4foBMLJWLxeS4dVDUrZB/view?usp=sharing'>Link to Resume Here</button>
+            <h1 className='d-flex justify-content-center profs mb-5'>Proficiencies</h1>
                 <div className='ul-container d-flex justify-content-between'>
                     <ul>
-                        <li>JavaScript</li>
-                        <li>React</li>
-                        <li>Express.js</li>
-                    </ul>
-                    <ul>
-                        <li>GraphQl</li>
                         <li>MongoDB</li>
-                        <li>CSS/Bootstrap</li>
+                        <li>Express.js</li>
+                        <li>React</li>
+                        <li>Node.js</li>
+                        <li>JavaScript</li>
+                        <li>State</li>
+                        <br></br>
                     </ul>
                     <ul>
-                        <li>One Thing</li>
-                        <li>One Thing</li>
-                        <li>One Thing</li>
+                        <li>MongoDB</li>
+                        <li>HTML</li>
+                        <li>CSS/Bootstrap</li>
+                        <li>GraphQl</li>
+                        <li>SQL</li>
+                        <li>Model-View-Controller</li>
+                    </ul>
+                    <ul>
+                        <li>Web APIs</li>
+                        <li>Third Party APIs</li>
+                        <li>Server-Side APIs</li>
+                        <li>Object Oriented Programming</li>
+                        <li>Progressive Web Applications</li>
+                        <li>Leadership & Organizational Skills</li>
                     </ul>
                 </div>
         </div>
