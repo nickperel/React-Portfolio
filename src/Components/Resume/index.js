d62c08303a963f0ff8bc870a628a624253d9a386
Kakashi <nickperel@live.com>
2022-04-09 18:51:25 -0400

    updated link to reflect updated resume

--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -7,7 +7,7 @@ function Resume() {
             <section id="header-img-partial"></section>
 
             <div className="container resume-container">
-                <a href='https://drive.google.com/file/d/1coYzl3u5rv3fvkWBVsmcEGKsHRfWKZQ6/view?usp=sharing' target="_blank" rel='noreferrer'><button className="btn btn-info btn-lg mt-5 mb-5">Link to Resume Here</button></a>
+                <a href='https://drive.google.com/file/d/1-3GUc-pcWoLp1wyMRhcyTwC4-T4l80bS/view?usp=sharing' target="_blank" rel='noreferrer'><button className="btn btn-info btn-lg mt-5 mb-5">Link to Resume Here</button></a>
                 <h1 className='d-flex justify-content-center profs mb-5'>Proficiencies</h1>
                     <div className='ul-container d-flex justify-content-between'>
                         <ul className='mt-3 mb-5 ml-5'>
