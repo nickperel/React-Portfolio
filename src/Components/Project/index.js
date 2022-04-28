77bd196c10b434c8f6f26ee85ed152f3d2738101
Kakashi <nickperel@live.com>
2022-04-27 22:01:57 -0400

    changed color theme to Chill Olive

--- src/Components/Project/index.js
+++ src/Components/Project/index.js
@@ -12,8 +12,8 @@ function Project({work}) {
                 <div id="work-main-img">
                     <div class={work.class}>
                         <div class="pic-label">
-                            <button className="btn btn-warning btn-lg mb-3 mx-3" onClick={() => handleClick(work.url)}>{work.title}</button>
-                            <button className="btn btn-warning btn-lg mb-3" onClick={() => handleClick(work.github)}>GitHub Repo</button>
+                            <button className="btn btn-success btn-lg mb-3 mx-3" onClick={() => handleClick(work.url)}>{work.title}</button>
+                            <button className="btn btn-success btn-lg mb-3" onClick={() => handleClick(work.github)}>GitHub Repo</button>
                         </div>
                     </div>
                 </div>
@@ -23,8 +23,8 @@ function Project({work}) {
             <div className="click-container">
                 <div class={work.class} id={work.id}>
                     <div class="pic-label">
-                        <button className="btn btn-warning mx-3" onClick={() => handleClick(work.url)}>{work.title}</button>
-                        <button className="btn btn-warning" onClick={() => handleClick(work.github)}>GitHub Repo</button>
+                        <button className="btn btn-success mx-3" onClick={() => handleClick(work.url)}>{work.title}</button>
+                        <button className="btn btn-success" onClick={() => handleClick(work.github)}>GitHub Repo</button>
                     </div>
                 </div>
             </div>
