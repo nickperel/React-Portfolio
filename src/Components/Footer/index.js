55bec2a29989d7174dc55689f76690b1b77a8510
Kakashi <nickperel@live.com>
2022-04-27 22:39:04 -0400

    changed link redirection in footer icons to open a new window when clicked

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer(props) {
         <div class="footer-basic" id="contact-me">
             <footer>
               <div class="column"></div>
-                <div class="social"><a href="https://www.instagram.com/nickperel/"><i class="icon ion-social-instagram"></i></a><a href="https://www.linkedin.com/in/nick-perel/"><i class="icon ion-social-linkedin"></i></a><a href="https://github.com/nickperel"><i class="icon ion-social-github"></i></a></div>
+                <div class="social"><a href="https://www.instagram.com/nickperel/" target="_blank" rel='noreferrer'><i class="icon ion-social-instagram"></i></a><a href="https://www.linkedin.com/in/nick-perel/" target="_blank" rel='noreferrer'><i class="icon ion-social-linkedin"></i></a><a href="https://github.com/nickperel" target="_blank" rel='noreferrer'><i class="icon ion-social-github"></i></a></div>
                 <ul class="list-inline">
                     <li><a href="mailto:nickperel@live.com">Email</a></li>
                     {/* <br /> */}
